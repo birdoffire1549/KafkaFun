b86d6e871d3487edc987474a023593ff04943ec4
Scott Griffis <scottgriffis@solutionary.com>
2016-12-20 21:57:42 -0600

    Added new features to make testers more robust

--- src/main/java/com/firebirdcss/sandbox/kafka/kafka_fun/consumer/KafkaReader.java
+++ src/main/java/com/firebirdcss/sandbox/kafka/kafka_fun/consumer/KafkaReader.java
@@ -14,10 +14,19 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
  *
  */
 public class KafkaReader {
-	private static final String KAFKA_HOST = "192.168.1.101";
-	private static final String KAFKA_PORT = "9092";
-	private static final String TOPIC = "my-topic";
-	private static final String GROUP_SEQ = "1";
+	private static final String KAFKA_HOST_DEFAULT = "192.168.1.101";
+	private static final String KAFKA_PORT_DEFAULT = "9092";
+	private static final String[] TOPICS_DEFAULT = new String[] {"my-topic"};
+	private static final String GROUP_NAME_DEFAULT = "group1";
+	
+	private static boolean showMessages = false;
+	
+	private static String kafkaHost = KAFKA_HOST_DEFAULT;
+	private static String kafkaPort = KAFKA_PORT_DEFAULT;
+	private static String groupName = GROUP_NAME_DEFAULT;
+	private static String[] topics = TOPICS_DEFAULT;
+	
+	private static KafkaConsumer<String, String> consumer = null;
 	
 	/**
 	 * APPLICATION MAIN: Main entry-point for the application.
@@ -25,41 +34,115 @@ public class KafkaReader {
 	 * @param args - Arguments as {@link String}[]
 	 */
 	public static void main(String[] args) {
-		String group = (args != null && args.length > 0 ? args[0] : GROUP_SEQ);
-		String host = (args != null && args.length > 1 ? args[1] : KAFKA_HOST);
+		handleProgramArgs(args);
 		
-		System.out.println("Starting up with Kafka host of: " + host);
+		System.out.println("Starting up with Kafka host of: " + kafkaHost + " and a port of: " + kafkaPort);
+		init();
 		
-		Properties props = new Properties();
-		props.put("bootstrap.servers", host + ":" + KAFKA_PORT);
-		props.put("group.id", "group" + group);
-		props.put("enable.auto.commit", "true");
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-		
-		System.out.println("Attempting to subscribe to assigned topic...");
-		consumer.subscribe(Arrays.asList(TOPIC));
+		System.out.println("Attempting to subscribe to assigned topic(s)...");
+		consumer.subscribe(Arrays.asList(topics));
 		System.out.println("Topic subscription complete.");
 		
 		boolean running = true;
 		System.out.println("Kafka Reader has started. Results will display as data is provided to the topic:");
 		
-		while (running) {
-			ConsumerRecords<String, String> records = consumer.poll(100);
-			for (ConsumerRecord<String, String> record : records) {
-				System.out.printf(": Record Received with offset = %d and value = %s%n", record.offset(), record.value());
-			}
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException ok) {
-				running = false;
-			}
+		long recordCount = 0;
+		if (!showMessages) {
+			System.out.println("Messages Consumed:");
+			System.out.print(recordCount);
 		}
-		
+		while (running) {
+			ConsumerRecords<String, String> records = consumer.poll(100/*TimeOutMillis*/);
+			if (records.isEmpty()) {
+				try {	
+					Thread.sleep(500);
+				} catch (InterruptedException ok) {
+					running = false;
+				}
+			} else {
+				for (ConsumerRecord<String, String> record : records) {
+					recordCount ++;
+					if (showMessages) {
+						System.out.printf(": Record Received with offset = %d and value = %s%n", record.offset(), record.value());
+					}
+				}
+			}
+			if (!showMessages) System.out.print("\r" + recordCount);
+		}
 		consumer.close();
 		System.out.println("\n~ Application Ended ~");
+	}
+	
+	/**
+	 * Initializes the application.
+	 * 
+	 * @param args
+	 */
+	private static void init() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", kafkaHost + ":" + kafkaPort);
+		props.put("group.id", groupName);
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "500");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		
+		consumer = new KafkaConsumer<>(props);
+	}
+	
+	/**
+	 * Parses Application arguments and setup the application to run as specified.
+	 * 
+	 * @param args
+	 */
+	private static void handleProgramArgs(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+				case "-s":
+				case "--showMessages":
+					showMessages = true;
+					break;
+				case "-H":
+				case "--host":
+					kafkaHost = args[++i];
+					break;
+				case "-p":
+				case "--port":
+					kafkaPort = args[++i];
+					break;
+				case "-g":
+				case "--group":
+					groupName = args[++i];
+					break;
+				case "-t":
+				case "--topics":
+					topics = args[++i].split(","); /* CSV list of topics */
+					break;
+				case "-h":
+				case "--help":
+				default:
+					printProgramUsage();
+					break;
+			}
+		}
+	}
+	
+	/**
+	 * Prints the program usage to the screen for the user to see.
+	 * 
+	 */
+	private static void printProgramUsage() {
+		StringBuilder message = new StringBuilder();
+		message.append("program usage: ").append("program [options]").append('\n');
+		message.append("Options:\n");
+		message.append("  [-s | --showMessages]                 Causes every messages received to be displayed to console").append('\n');
+		message.append("  [-H | --host] <hostname>              Specifies the hostname of the kafka broker").append('\n');
+		message.append("  [-p | --port] <portnumber>            Specifies the port of the kafka broker").append('\n');
+		message.append("  [-g | --group] <groupname>            Specifies the consumer group to which the consumer belongs").append('\n');
+		message.append("  [-t | --topics] <topic1,topic2...>    A CSV list of topics to subscribe to").append('\n');
+		message.append("  [-h | --help]                         Displays this program usage page.").append('\n');
+		
+		System.out.println(message);
+		System.exit(1);
 	}
 }
--- src/main/java/com/firebirdcss/sandbox/kafka/kafka_fun/producer/KafkaSender.java
+++ src/main/java/com/firebirdcss/sandbox/kafka/kafka_fun/producer/KafkaSender.java
@@ -1,6 +1,7 @@
 package com.firebirdcss.sandbox.kafka.kafka_fun.producer;
 
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -14,20 +15,105 @@ import org.apache.kafka.clients.producer.ProducerRecord;
  *
  */
 public class KafkaSender {
-	private static final String KAFKA_HOST = "192.168.1.101";
-	private static final String KAFKA_PORT = "9092";
-	private static final String TOPIC = "my-topic";
+	private static final String KAFKA_HOST_DEFAULT = "192.168.1.101";
+	private static final String KAFKA_PORT_DEFAULT = "9092";
+	private static final String TOPIC_DEFAULT = "my-topic";
+	private static final int PUB_RATE_PER_SECOND_DEFAULT = 100000;
+	private static final int PAYLOAD_SIZE_BYTES_DEFAULT = 8192;
+	private static final boolean AUTO_PUBLISH_DEFAULT = false;
+	
+	private static boolean autoPublish = AUTO_PUBLISH_DEFAULT;
+	private static int pubRatePerSecond = PUB_RATE_PER_SECOND_DEFAULT;
+	
+	private static String kafkaHost = KAFKA_HOST_DEFAULT;
+	private static String kafkaPort = KAFKA_PORT_DEFAULT;
+	private static String topic = TOPIC_DEFAULT;
+	private static int payloadSizeBytes = PAYLOAD_SIZE_BYTES_DEFAULT;
+	
+	private static Producer<String, String> producer = null;
 	
 	/**
 	 * APPLICATION MAIN: The main entry-point of the application.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String host = (args != null && args.length > 0 ? args[0] : KAFKA_HOST);
-		System.out.println("Starting up with Kafka host of: " + host);
+		handleProgramArgs(args);
+		init();
+		System.out.println("Starting up with Kafka host of: " + kafkaHost);
 		
+		if (autoPublish) {
+			System.out.println("Prepairing for auto publish...");
+			String payload = generatePayload();
+			boolean running = true;
+			long messagesSentThisCycle = 0;
+			long totalMessagesSent = 0;
+			long firstSend = 0;
+			System.out.println("Auto publishing begginning.");
+			System.out.println("Sending messages that are '" + payload.length() + "' bytes in size.");
+			while(running) {
+				if (firstSend == 0) firstSend = System.currentTimeMillis();
+				producer.send(new ProducerRecord<String, String>(topic, payload));
+				totalMessagesSent ++;
+				System.out.print("\rMessages Sent: " + totalMessagesSent);
+				messagesSentThisCycle ++;
+				if (messagesSentThisCycle >= pubRatePerSecond) {
+					long sleepDiff = 0;
+					if ((sleepDiff = 1000 - (System.currentTimeMillis() - firstSend)) > 0) {
+						try {
+							Thread.sleep(sleepDiff);
+						} catch (InterruptedException ok) {
+							running = false;
+						}
+					}
+					firstSend = 0;
+					messagesSentThisCycle = 0;
+				}
+			}
+		} else {
+			Scanner sc = new Scanner(System.in);
+			String line = null;
+			System.out.println("Enter text to send to consumer (type 'exit' to quit):");
+			System.out.print("> ");
+			while (!(line = sc.nextLine()).equals("exit")) {
+				if (!line.equals("exit")) {
+					producer.send(new ProducerRecord<String, String>(topic, line));
+					System.out.print("> ");
+				}
+			}
+			
+			sc.close();
+		}
+		
+		producer.close();
+		System.out.println("\n ~ Application Ended ~");
+	}
+	
+	/**
+	 * TODO: This should generate a random payload of a specific size
+	 * 
+	 * @return Returns a payload as {@link String}
+	 */
+	private static String generatePayload() {
+		char[] payload = new char[payloadSizeBytes];
+		
+		System.out.println("Building a '" + payloadSizeBytes + "' byte payload for sending...");
+		Random r = new Random();
+		for (int i = 0; i < payloadSizeBytes; i++) {
+			payload[i] = (char) (r.nextInt(126 - 32) + 32);
+		}
+		System.out.println("Payload has been generated.");
+		
+		return String.copyValueOf(payload);
+	}
+	
+	/**
+	 * Initializes the application.
+	 * 
+	 * @param args
+	 */
+	private static void init() {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", host + ":" + KAFKA_PORT);
+		props.put("bootstrap.servers", kafkaHost + ":" + kafkaPort);
 		props.put("acks", "all");
 		props.put("retries", 0);
 		props.put("batch.size", 16384);
@@ -36,21 +122,75 @@ public class KafkaSender {
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-		Producer<String, String> producer = new KafkaProducer<>(props);
-		
-		Scanner sc = new Scanner(System.in);
-		String line = null;
-		System.out.println("Enter text to send to consumer (type 'exit' to quit):");
-		System.out.print("> ");
-		while (!(line = sc.nextLine()).equals("exit")) {
-			if (!line.equals("exit")) {
-				producer.send(new ProducerRecord<String, String>(TOPIC, line));
-				System.out.print("> ");
+		producer = new KafkaProducer<>(props);
+	}
+	
+	/**
+	 * Parses Application arguments and setup the application to run as specified.
+	 * 
+	 * @param args
+	 */
+	private static void handleProgramArgs(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+				case "-a":
+				case "--autoPublish":
+					autoPublish = true;
+					break;
+				case "-r":
+				case "--rateOfPublish":
+					try {
+						pubRatePerSecond = Integer.parseInt(args[++i]);
+					} catch (NumberFormatException bad) {
+						printProgramUsage();
+					}
+					break;
+				case "-H":
+				case "--host":
+					kafkaHost = args[++i];
+					break;
+				case "-s":
+				case "--payloadSize":
+					try {
+						payloadSizeBytes = Integer.parseInt(args[++i]);
+					} catch (NumberFormatException bad) {
+						printProgramUsage();
+					}
+					break;
+				case "-p":
+				case "--port":
+					kafkaPort = args[++i];
+					break;
+				case "-t":
+				case "--topic":
+					topic = args[++i];
+					break;
+				case "-h":
+				case "--help":
+				default:
+					printProgramUsage();
+					break;
 			}
 		}
+	}
+	
+	/**
+	 * Prints the program usage to the screen for the user to see.
+	 * 
+	 */
+	private static void printProgramUsage() {
+		StringBuilder message = new StringBuilder();
+		message.append("program usage: ").append("program [options]").append('\n');
+		message.append("Options:\n");
+		message.append("  [-a | --autoPublish]                  Autogenerates and publishes messages at the rate of 1 per second unless otherwise specified").append('\n');
+		message.append("  [-H | --host] <hostname>              Specifies the hostname of the kafka broker").append('\n');
+		message.append("  [-p | --port] <portNumber>            Specifies the port of the kafka broker").append('\n');
+		message.append("  [-r | --rateOfPublish] <number>       Specifies the number of auto generated messages to publish a second").append('\n');
+		message.append("  [-t | --topic] <topic>                The topic to which to publish messages").append('\n');
+		message.append("  [-s | --payloadSize] <numberBytes>    The number of bytes the auto generated message should be in size").append('\n');
+		message.append("  [-h | --help]                         Displays this program usage page.").append('\n');
 		
-		sc.close();
-		producer.close();
-		System.out.println("\n ~ Application Ended ~");
+		System.out.println(message);
+		System.exit(1);
 	}
 }
